3832c825170e683caf3645432c4baa96f60ce6de
abhav <abhav.thakur2018@vitstudent.ac.in>
2020-05-14 01:53:14 +0530

    dashboard new

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,18 @@ router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
-    user: req.user 
+    user: req.user , menuId:'home'
   })
 );
+router.get('/about', ensureAuthenticated, function(req, res, next) {
+  res.render('about', { menuId:'about'});
+});
 
+router.get('/contact', ensureAuthenticated, function(req, res, next) {
+  res.render('contact', { menuId:'contact'});
+});
+router.get('/service', function(req, res, next) {
+  res.render('service', { menuId:'service'});
+});
 
 module.exports = router;
