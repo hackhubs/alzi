2ebbea455e6d286159472e10c97e3c7a56798af8
Abhav Thakur <61389183+hackhubs@users.noreply.github.com>
2021-02-23 17:51:09 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://abhav:ab8988143226@cluster0-mtl3r.mongodb.net/test?retryWrites=true&w=majority",
+    "mongodb+srv://name:ab8988143226@cluster0-mtl3r.mongodb.net/test?retryWrites=true&w=majority",
 };
